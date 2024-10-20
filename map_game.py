4c1accf4153d600551f6943d360ce886f6ef5099
erfan891107 <erfan.naseri.89.11.07@gmail.com>
2024-10-20 15:10:48 +0330

    Update map_game.py

--- map_game.py
+++ map_game.py
@@ -17,39 +17,6 @@ class Map:
         for mane in self.levels[level_number][name]:
             pygame.draw.rect(display, RED, [mane.x, mane.y, mane.width, mane.height])    
 
-
-# wall_map_level_1 = [Rect ([0, 0, 1200, 50]),
-#                     Rect ([0, 550, 1200, 50]),
-#                     Rect ([1150, 50, 50, 550]),
-#                     Rect ([0, 0, 50, 550]),
-#                     Rect ([0, 100, 950, 50]),
-#                     Rect ([1050, 100, 50, 200]),
-#                     Rect ([900, 250, 200, 50]),
-#                     Rect ([900, 150, 50, 150]),
-#                     Rect ([750, 350, 400, 50]),
-#                     Rect ([500, 300, 250, 100]),
-#                     Rect ([50, 300, 350, 150]),
-#                     Rect ([500, 400, 200, 50]),
-#                     Rect ([150, 200, 750, 50]),
-#                     Rect ([800, 450, 350, 100]),
-#                     Rect ([750, 500, 50, 50]),
-#                     Rect ([775, 475, 25, 25]),
-#                     Rect ([725, 525, 25, 25]),
-#                     Rect ([750, 325, 25, 25])]
-
-# wall_1_map_level_1 = None
-
-# mane_1_map_level_1 = [Rect ([950, 200, 150, 50])]
-#                       #Rect ([950, 150, 25, 50]),
-#                       #Rect ([975, 175, 25, 25])
-
-# bridge_1_map_level_1 = [Rect ([950, 100, 100, 25])]
-
-# mane_2_map_level_1 = [Rect ([50, 250, 25, 50]),
-#                       Rect ([75, 275, 25, 25])]
-
-# bridge_2_map_level_1 = [Rect ([125, 225, 25, 25])]
-
 map = Map()
 map.add_level(1)
 map.levels[1]["wall"] = [Rect ([0, 0, 1200, 50]),
