c42b271106ef13e0e03b7ddbcff11425f5f2da6c
erfan891107 <erfan.naseri.89.11.07@gmail.com>
2024-10-25 11:50:17 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import pygame
 from map_game import map
 from pygame.locals import Rect
+#pygame.font.init()
 
 #display
 
 WIDTH, HEIGHT = 1200, 600
 display = pygame.display.set_mode((WIDTH, HEIGHT))
-
-#gamer_skin
 
 #colors
 
@@ -18,19 +17,34 @@ GREEN = (0, 255, 0)
 x, y = 75, 75
 
 gamer = Rect ([x, y, 25, 25])
+coin = Rect ([850, 165, 25, 25])
+gun_trap = Rect ([55, 520, 100, 25])
+finish_level_1 = Rect ([1075, 396, 60, 60])
 
 clock = pygame.time.Clock()
 
 x_change, y_change = 0, 0
 
-has_power = True
 show_bridge = False
-space_power = False
+
+space_power_1 = True
+space_power_2 = True
+space_power_3 = True
+
+#score_number_1 = pygame.font.SysFont("comicsens", 25)
 
 game = True
 
 x, y = 0, 0
 
+score = pygame.image.load("score.PNG")
+score = pygame.transform.scale(score,(25, 25))
+
+trap = pygame.image.load("trap.PNG")
+trap = pygame.transform.scale(trap,(100, 25))
+
+finish = pygame.image.load("finish.PNG")
+finish = pygame.transform.scale(finish,(60, 60))
 
 def plot_player(direction):
     player = pygame.image.load(f"player {direction}.PNG")
@@ -42,7 +56,6 @@ def collide_player_rects(gamer, rects):
         if pygame.Rect.colliderect(gamer, mane):
             return True
     return False
-
 
 def x_change_gamer():
     gamer.x -= x_change
@@ -73,7 +86,7 @@ while game:
     keys = pygame.key.get_pressed()
 
     # RIGHT
-    if keys[pygame.K_RIGHT] :
+    if keys[pygame.K_d] :
         gamer.x += 5
         gamer.y += 0
   
@@ -81,7 +94,7 @@ while game:
         y_change = 0
         direction = "right"
     # LEFT
-    if keys[pygame.K_LEFT] :
+    if keys[pygame.K_a] :
         gamer.x += -5
         gamer.y += 0
 
@@ -91,19 +104,16 @@ while game:
 
     player = plot_player(direction)
 
-    if keys[pygame.K_e]:
-        if has_power:
+    if space_power_1 == True and keys[pygame.K_e] and direction == "right":
+    
+        gamer.x += 150
+        x_change = 150
+        space_power_1 = False
 
-            for i in range (150):
-                if direction == "right":
-                    gamer.x += 1
-                    x_change = 1
-                else:
-                    gamer.x -= 1
-                    x_change = -1
-
-            has_power = False
-
+    if space_power_1 == True and keys[pygame.K_e] and direction == "left":
+        gamer.x -= 150
+        x_change = -150
+        space_power_1 = False
 
     gamer.x %= WIDTH
     gamer.y %= HEIGHT
@@ -128,31 +138,30 @@ while game:
 
         x_change_gamer() 
 
-
-
     for i in range (5) :
         gamer.y += 1
     
     if keys[pygame.K_SPACE] and collide_player_rects(gamer,  map.levels[1]["wall"]):
         space_xy()
                     
-    for mane in map.levels[1]["mane_1"]:
-        if 1024 < gamer.x and 1050 > gamer.x :
-            space_power = True
-        if space_power == True and  keys[pygame.K_c] and pygame.Rect.colliderect(gamer, mane) :
-                
-            gamer.x += 0
-            gamer.y += -100
+    if space_power_2 == True and  keys[pygame.K_SPACE] and collide_player_rects(gamer, map.levels[1]["mane_1"]) and 1024 < gamer.x and 1050 > gamer.x :
+            
+        gamer.x += 0
+        gamer.y += -100
+        x_change = 0
+        y_change = -100 
 
-            x_change = 0
-            y_change = -100 
+        space_power_2 = False
 
-            space_power = False
+    if space_power_3 == True and  keys[pygame.K_SPACE] and collide_player_rects(gamer,  map.levels[1]["mane_2"]) and 50 < gamer.x and 100 > gamer.x and direction == "right":
 
-        if pygame.Rect.colliderect(gamer, mane) :
-            if keys[pygame.K_SPACE]:
-                space_xy() 
+        gamer.x += 75
+        gamer.y += -75
+        x_change = 75
+        y_change = -75
 
+    if keys[pygame.K_SPACE] and collide_player_rects(gamer, map.levels[1]["mane_1"]):
+        space_xy()
 
     if keys[pygame.K_SPACE] and collide_player_rects(gamer, map.levels[1]["bridge_1"]):
         space_xy()
@@ -163,13 +172,10 @@ while game:
     if keys[pygame.K_SPACE] and collide_player_rects(gamer, map.levels[1]["bridge_2"]):
         space_xy()
       
-    if collide_player_rects(gamer,  map.levels[1]["mane_1"]):
-            if gamer.x >= 950 :
-                show_bridge = True
- 
-    if show_bridge:
-        for mane in map.levels[1]["bridge_1"] :
-            pygame.draw.rect(display, RED, [mane.x, mane.y, mane.width, mane.height])
+    if collide_player_rects(gamer,  map.levels[1]["mane_1"]) and gamer.x >= 950 :
+        show_bridge = True
+    if show_bridge == True :
+        map.draw_rects(display, 1, "bridge_1")
 
     if collide_player_rects(gamer,  map.levels[1]["wall"]):
         not_gravity()
@@ -177,23 +183,30 @@ while game:
     if collide_player_rects(gamer,  map.levels[1]["mane_1"]):
         not_gravity()
 
-    if show_bridge and collide_player_rects(gamer,  map.levels[1]["bridge_1"]):
+    if show_bridge == True and collide_player_rects(gamer,  map.levels[1]["bridge_1"]):
         not_gravity()
 
     if collide_player_rects(gamer,  map.levels[1]["mane_2"]):
         not_gravity()
 
     if collide_player_rects(gamer,  map.levels[1]["bridge_2"]):
-        not_gravity()
+        not_gravity()    
 
+    #if collide_player_rects(gamer,  map.levels[1]["score"]):
+
+
+    if collide_player_rects(gamer,  map.levels[1]["finish"]):
+        game = False
 
     map.draw_rects(display, 1, "wall")
     map.draw_rects(display, 1, "mane_1")
     map.draw_rects(display, 1, "mane_2")
     map.draw_rects(display, 1, "bridge_2")
 
-
     display.blit(player, [gamer.x, gamer.y])
+    display.blit(score, [coin.x, coin.y])
+    display.blit(trap, [gun_trap.x, gun_trap.y])
+    display.blit(finish, [finish_level_1.x, finish_level_1.y])
 
     pygame.display.update()
 
