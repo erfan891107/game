2c7359d2dbd5d976cef674e5a333cebd46d4ff58
erfan891107 <erfan.naseri.89.11.07@gmail.com>
2024-10-25 11:51:29 +0330

    Update map_game.py

--- map_game.py
+++ map_game.py
@@ -17,6 +17,18 @@ class Map:
         for mane in self.levels[level_number][name]:
             pygame.draw.rect(display, RED, [mane.x, mane.y, mane.width, mane.height])    
 
+class Map:
+    def __init__(self):
+        self.levels = {}
+
+    def add_level(self, level_name):
+        self.levels[level_name] = {}
+
+    def draw_rects(self, display, level_number, name):
+
+        for mane in self.levels[level_number][name]:
+            pygame.draw.rect(display, RED, [mane.x, mane.y, mane.width, mane.height]) 
+
 map = Map()
 map.add_level(1)
 map.levels[1]["wall"] = [Rect ([0, 0, 1200, 50]),
@@ -31,7 +43,7 @@ map.levels[1]["wall"] = [Rect ([0, 0, 1200, 50]),
                     Rect ([500, 300, 250, 100]),
                     Rect ([50, 300, 350, 150]),
                     Rect ([500, 400, 200, 50]),
-                    Rect ([150, 200, 750, 50]),
+                    Rect ([175, 200, 725, 50]),
                     Rect ([800, 450, 350, 100]),
                     Rect ([750, 500, 50, 50]),
                     Rect ([775, 475, 25, 25]),
@@ -41,11 +53,15 @@ map.levels[1]["wall"] = [Rect ([0, 0, 1200, 50]),
 
 map.levels[1]["mane_1"] =  [Rect ([950, 200, 150, 50])]
 
-map.levels[1]["mane_2"] =  [Rect ([50, 250, 25, 50]),
-                            Rect ([75, 275, 25, 25])]
+map.levels[1]["mane_2"] =  [Rect ([50, 250, 50, 50]),
+                            Rect ([100, 275, 25, 25])]
 
 map.levels[1]["bridge_1"] = [Rect ([950, 100, 100, 25])]
 
-map.levels[1]["bridge_2"] = [Rect ([125, 225, 25, 25])]
+map.levels[1]["bridge_2"] = [Rect ([150, 225, 25, 25])]
 
+map.levels[1]["score"] = [Rect ([850, 165, 25, 25])]
 
+map.levels[1]["finish"] = [Rect ([1075, 400, 50, 50])]
+
+map.levels[1]["trap"] = [Rect ([55, 500, 100, 25])]
